78429aa0f54b86209a5911cfaac95764a08cc685
jakisondev <jakison.dev@gmail.com>
2021-10-18 09:03:32 -0300

    CustomCalendar

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ import {
 import { Home } from './src/screens/Home';
 import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 
 export default function App() {
@@ -35,7 +36,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   )
 }
