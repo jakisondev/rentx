f876a03dfff6f1c37dc3ab1fa466053807d5ecbd
jakisondev <jakison.dev@gmail.com>
2021-11-11 12:13:01 -0300

    UpdateProfile

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -23,6 +23,7 @@ interface AuthContextData {
     user: User;
     signIn: (credentials: SignInCredentials) => Promise<void>;
     signOut: () => Promise<void>;
+    updateUser: (user: User) => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -76,6 +77,24 @@ function AuthProvider({ children }: AuthProviderProps) {
         }
     }
 
+    async function updateUser(user: User) {
+        try {
+            const userCollection = database.get<ModelUser>('users');
+            await database.action(async () => {
+                const userSelected = await userCollection.find(user.id);
+                await userSelected.update((userData) => {
+                    userData.name = user.name;
+                    userData.driver_license = user.driver_license;
+                    userData.avatar = user.avatar;
+                });
+            });
+
+            setData(user);
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
+
     useEffect(() => {
         async function loadUserData() {
             const userCollection = database.get<ModelUser>('users');
@@ -92,7 +111,7 @@ function AuthProvider({ children }: AuthProviderProps) {
     });
 
     return (
-        <AuthContext.Provider value={{ user: data, signIn, signOut }}>
+        <AuthContext.Provider value={{ user: data, signIn, signOut, updateUser }}>
             {children}
         </AuthContext.Provider>
     )
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import * as Yup from 'yup';
 
 import { useTheme } from 'styled-components';
 import { useNavigation } from '@react-navigation/core';
@@ -12,6 +13,8 @@ import { Feather } from '@expo/vector-icons';
 import { BackButton } from '../../components/BackButton';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+
+import { Button } from '../../components/Button';
 
 import {
     Container,
@@ -30,7 +33,7 @@ import {
 } from './styles';
 
 export function Profile() {
-    const { user, signOut } = useAuth();
+    const { user, signOut, updateUser } = useAuth();
     const theme = useTheme();
     const navigation = useNavigation();
 
@@ -60,6 +63,40 @@ export function Profile() {
         }
         if (result.uri) {
             setAvatar(result.uri);
+        }
+    }
+
+    async function handleProfileUpdate() {
+        try {
+            const schema = Yup.object().shape({
+                driver_license: Yup.string()
+                    .required('CNH é obrigatória'),
+                name: Yup.string()
+                    .required('Nome é obrigatório')
+            });
+
+            const data = { name, driverLicense };
+
+            await schema.validate(data);
+
+            await updateUser({
+                id: user.id,
+                user_id: user.user_id,
+                email: user.email,
+                name,
+                driver_license: driverLicense,
+                avatar,
+                token: user.token
+            });
+
+            Alert.alert('Perfil atualizado');
+
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                Alert.alert('Opa', error.message);
+            } else {
+                Alert.alert('Não foi possível atualizar o perfil');
+            }
         }
     }
 
@@ -123,6 +160,10 @@ export function Profile() {
                                 <PasswordInput iconName="lock" placeholder="Repetir senha" />
                             </Section>
                         }
+                        <Button
+                            title="Salvar alterações"
+                            onPress={handleProfileUpdate}
+                        />
                     </Content>
                 </Container>
             </TouchableWithoutFeedback>
